34a41b99c6eb341bf48207d5d984a92fbd15ae4b
Guglielmo Marra <156308467+marraguglielmo@users.noreply.github.com>
2024-03-20 15:34:23 +0100

    [add] css, golbal and scoped

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,38 @@
 export default {
   data(){
     return{
-      title:'Titolo primo Vite',
-      p: 'primo paragrafo Vite'
+      title:'Titolo globale Vite',
+      p: 'primo paragrafo Vite',
+      title2: 'Titolo scope Vite',
+      p2: 'paragrafo css scope'
     }
   }
 }
 </script>
 
 <template>
-  <div>
-    <h1>{{ title }}</h1>
-    <p>{{ p }}</p>
+  <div class="container-md d-flex">
+    <!-- container css globale -->
+    <div class="box box1">
+      <h1>{{ title }}</h1>
+      <p>{{ p }}</p>
+    </div>
+    <!-- container css scoped -->
+    <div class="box box2">
+      <h1>{{ title2 }}</h1>
+      <p>{{ p2 }}</p>
+    </div>
   </div>
 </template>
 
-<style>
+<style scoped>
+  .box.box2 h1{
+    color: rgb(145, 49, 235);
+  }
+
+  .box.box2 p{
+    color: rgb(24, 211, 211);
+    font-size: 1.7rem;
+  }
 
 </style>
